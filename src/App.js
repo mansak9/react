2ccb0f54b4359cb4e051cc124d395c1f73300f83
parkminsik <mansak9@naver.com>
2019-01-28 15:07:28 +0900

    Revert "initial project"
    This reverts commit 2877d585a3bfd46c15c6b2f098d061879252668e.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -11,7 +11,12 @@ class App extends Component {
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
-          <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             Learn React
           </a>
         </header>
