808c32e3d46381df25443eeabaa9e314f2a6ddbd
parkminsik <mansak9@naver.com>
2019-01-29 17:56:17 +0900

    no message

--- src/App.js
+++ src/App.js
@@ -13,11 +13,46 @@ const data = [
   { name: "PR 코드리뷰를 응용한 개발 프로세스를 익힌다.", completed: true },
   { name: "React로 간단한 노트앱을 만들어 본다.", completed: true }
 ];
+
 class App extends Component {
+  state = {
+    data,
+    isUnmount: false
+  };
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({
+        data: [
+          { name: "React 개발에 필요한 환경을 구축한다.", completed: true },
+          {
+            name: "새로운 자바스크립트 문법을 익힌다. ES2015(ES6), ES7, ES8",
+            completed: true
+          },
+          { name: "개발 편의를 위한 VSCode IDE를 익힌다.", completed: true },
+          { name: "기본적인 Git사용법을 익힌다.", completed: true },
+          {
+            name: "PR 코드리뷰를 응용한 개발 프로세스를 익힌다.",
+            completed: true
+          },
+          { name: "React로 간단한 노트앱을 만들어 본다.", completed: true }
+        ]
+      });
+    }, 5000);
+
+    setTimeout(() => {
+      this.setState({
+        isUnmount: true
+      });
+    }, 10000);
+  }
+
   render() {
     return (
       <div className="App">
-        <Todos title={"강의 목표"} items={data} />
+        {!this.state.isUnmount && (
+          <Todos title={"강의 목표"} items={this.state.data} />
+        )}
       </div>
     );
   }
--- src/componets/Todos.jsx
+++ src/componets/Todos.jsx
@@ -1,27 +1,30 @@
 import React from "react";
 
-const Todos = (props) => {
-  console.log('----->', props);
+class Todos extends React.Component{
 
-  return (
-    <div className="Todos" style={{color: "blue"}}>
-      <h2>{props.title}</h2>
-      <ul>
-       {/* <li><input type="checkbox" checked/>{props.items[0]}</li>
-        <li><input type="checkbox"/>{props.items[1]}</li>
-        <li><input type="checkbox"/>{props.items[2]}</li>
-        <li><input type="checkbox"/>{props.items[3]}</li>
-        <li><input type="checkbox"/>{props.items[4]}</li>
-        <li><input type="checkbox"/>{props.items[5]}</li>*/}
+  componentDidMount() {
+    console.log('componentDidMount')
+  }
 
+  componentWillUnmount() {
+    console.log('componentDidMount')
+  }
 
-        {props.items.map((item, index) =>{
+  render() {
+    const { title, items } = this.props;
+
+    return (
+      <div className="Todos" style={{color: "blue"}}>
+        <h2>{title}</h2>
+        <ul>
+          {items.map((item, index) => {
             return <li key={index}><input type="checkbox" checked={item.completed}/>{item.name}</li>
-        })}
+          })}
 
-      </ul>
-    </div>
-  );
+        </ul>
+      </div>
+    );
+  }
 
 };
 
