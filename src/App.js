0f1a40c3ad7abc9bc8102e5360ecf15a4764ead4
parkminsik <mansak9@naver.com>
2019-01-29 16:03:15 +0900

    no message

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React, { Component } from "react";
-// import logo from "./logo.svg";
 import "./App.css";
-import LectureGoalList from "./componets/LectureGoalList";
+import Todos from "./componets/Todos";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <LectureGoalList />
+        <Todos
+          title={"강의 목표"}
+          items={[
+            "React 개발에 필요한 환경을 구축한다.",
+            "새로운 자바스크립트 문법을 익힌다. ES2015(ES6), ES7, ES8",
+            "개발 편의를 위한 VSCode IDE를 익힌다.",
+            "기본적인 Git사용법을 익힌다.",
+            "PR 코드리뷰를 응용한 개발 프로세스를 익힌다.",
+            "React로 간단한 노트앱을 만들어 본다."
+          ]}
+        />
       </div>
     );
   }
---
+++ src/componets/Todos.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Todos = (props) => {
+  console.log('----->', props);
+
+  return (
+    <div className="Todos" style={{color: "blue"}}>
+      <h2>{props.title}</h2>
+      <ul>
+       {/* <li><input type="checkbox" checked/>{props.items[0]}</li>
+        <li><input type="checkbox"/>{props.items[1]}</li>
+        <li><input type="checkbox"/>{props.items[2]}</li>
+        <li><input type="checkbox"/>{props.items[3]}</li>
+        <li><input type="checkbox"/>{props.items[4]}</li>
+        <li><input type="checkbox"/>{props.items[5]}</li>*/}
+
+        {props.items.map((item, index) =>{
+          return <li key={index}><input type="checkbox"/>{item}</li>
+        })}
+
+      </ul>
+    </div>
+  );
+
+};
+
+export default Todos;
